b545c31e8b2dbbf398e1c67afd58bbc1bdaf2ed1
greg <greg.micah.jensen@gmail.com>
2026-05-18 19:04:46 -0400

    adds docker-compose.yml validation

--- src/agent.py
+++ src/agent.py
@@ -5,3 +5,6 @@ def validate_agent_dir(agent_dir: str):
     init_sh = Path(agent_dir) / "init.sh"
     if not init_sh.exists():
         raise ValueError()
+    docker_compose = Path(agent_dir) / "docker" / "docker-compose.yml"
+    if not docker_compose.exists():
+        raise ValueError()
--- src/test/agent_test.py
+++ src/test/agent_test.py
@@ -7,3 +7,10 @@ def test__validate_agent_dir__missing_init_sh(fs):
 
     with pytest.raises(ValueError):
         sut.validate_agent_dir("/agent")
+
+
+def test__validate_agent_dir__missing_docker_compose(fs):
+    fs.create_file("/agent/init.sh", contents="")
+
+    with pytest.raises(ValueError):
+        sut.validate_agent_dir("/agent")
